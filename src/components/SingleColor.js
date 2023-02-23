cf52706140f9ccb349a793845873cea528bc5d35
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-23 23:43:00 +0100

    created class with same name of obj props

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -3,7 +3,12 @@ import { rgbToHex } from '../utilities/helper'
 
 const SingleColor = ({rgb, type, weight}) => {
   return (
-    <div className='col-4 d-flex justify-content-center align-items-center box' style={{ backgroundColor : rgbToHex(...rgb) }}>
+    // create 3 classi con lo stesso nome dei valori di type per gestire il nome del colore e fare in modo che si legge con qualsiasi colore/sfumatura.
+    // Passiamo type tra le classi
+    <div className={`col-4 d-flex justify-content-center align-items-center box ${type}`} 
+    // passiamo rgb con lo spread operator poichè rgb è un array 
+    style={{ backgroundColor : rgbToHex(...rgb) }}>
+
         <span>{rgbToHex(...rgb)}</span>
     </div>
   )
