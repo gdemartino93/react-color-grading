25e8e44bf9c6daa40d778d8c13772dbfdbfdc614
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-23 17:31:07 +0100

    working on  color input state

--- src/components/ColorGrading.js
+++ src/components/ColorGrading.js
@@ -3,11 +3,46 @@ import React , { useState } from 'react';
 
 
 const ColorGrading = () => {
+  const [colorInput , setColorInput] = useState({
+    color : "",
+    qty : 10
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("inviato")
+  }
+  const handleChange = (e) => {
+    const { name , value} = e.target;
+    setColorInput({...colorInput,
+    [name]: value,})
+    
+    console.log(e.target.value)
+  }
+
   return <>
-    <form className='d-flex align-items-center gap-2 p-3'>
+    <form className='d-flex align-items-center gap-2 p-3' onSubmit={handleSubmit} >
         <div className='d-flex gap-3'>
-            <input type="text" value="0" ></input>
-            <input type="text" value="0" ></input>
+            <input
+              type="text"
+              id='color'
+              name='color'
+              value={colorInput.color}
+              maxLength={7}
+              onChange={handleChange}
+              >
+            </input>
+            <input
+             type="number"
+             name='qty'
+             id='qty'
+             min={5}
+             max={100}
+             step={5}
+             value={colorInput.qty} 
+             onChange = {handleChange}
+             >
+
+             </input>
         </div>
         <button className='btn btn-success' type='submit'> Select</button>
 
