4fefee245a371e889dafc8ec4f76ef6bf1028db4
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-23 23:32:39 +0100

    new library react for key on child

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -3,7 +3,7 @@ import { rgbToHex } from '../utilities/helper'
 
 const SingleColor = ({rgb, type, weight}) => {
   return (
-    <div className='col-4' style={{ backgroundColor : rgbToHex(...rgb) }}>
+    <div className='col-4 d-flex justify-content-center align-items-center box' style={{ backgroundColor : rgbToHex(...rgb) }}>
         <span>{rgbToHex(...rgb)}</span>
     </div>
   )
