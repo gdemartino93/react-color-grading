4638eab4add46ae46c0cac00a632b3b29ed77a7e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-23 22:39:32 +0100

    handle submit and array with palette

--- src/components/ColorGrading.js
+++ src/components/ColorGrading.js
@@ -1,26 +1,36 @@
 import React , { useState } from 'react';
 import { v4 } from 'uuid';
 import Values from 'values.js'
+import SingleColor from './SingleColor';
 
 
 const ColorGrading = () => {
-  const color = new Values('rgba(0,0,0,0.5)');
-  console.log(color);
+
   const [colorInput , setColorInput] = useState({
     color : "red",
-    qty : 5
-  })
+    qty : 0
+  });
+  const [selectedColor , setSelectedColor] = useState([]);
+
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if ( colorInput.color && colorInput.qty){
+      const {color , qty } = colorInput;
+      setSelectedColor(
+        new Values(color).all(Math.round(100 / parseInt(qty,10)) * 2 )
+      )  
+    }
+
   }
   const handleChange = (e) => {
     const { name , value} = e.target;
     setColorInput({...colorInput,
     [name]: value,})
-    
-
+  
   }
-
+  console.log(selectedColor);
   return <>
     <form className='d-flex align-items-center gap-2 p-3' onSubmit={handleSubmit} >
         <div className='d-flex gap-3'>
@@ -47,8 +57,14 @@ const ColorGrading = () => {
              </input>
         </div>
         <button className='btn btn-success' type='submit'> Select</button>
-
     </form>
+    <div className='container'>
+        {
+          selectedColor > 0 ?
+          (selectedColor.map(el => <SingleColor key={v4} {...el} />))
+          : "Loading..."
+        }
+    </div>
   </>
 }
 
