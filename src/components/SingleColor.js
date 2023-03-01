b9135bb05741c57ce16863faf0f30a7e4008b8ab
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-01 19:07:23 +0100

    copy to clipboard

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { rgbToHex } from '../utilities/helper'
 
 const SingleColor = ({rgb, type, weight}) => {
+  const [textColor , setTextColor] = useState(false);
+  // creiamo la funzione per copiare il colore
+  const copiaColore = () => {
+    navigator.clipboard
+      .writeText(rgbToHex(...rgb))
+      // se la promise ha successo cambiamo lo stato in true
+      .then(()=> setTextColor(true))
+      .catch(err => console.log(err));
+  };
   return (
     // create 3 classi con lo stesso nome dei valori di type per gestire il nome del colore e fare in modo che si legge con qualsiasi colore/sfumatura.
     // Passiamo type tra le classi
@@ -10,7 +19,7 @@ const SingleColor = ({rgb, type, weight}) => {
     // passiamo rgb con lo spread operator poichè rgb è un array 
     style={{ backgroundColor : rgbToHex(...rgb) }}>
 
-        <span>{rgbToHex(...rgb)}</span>
+        <span style={{ cursor : 'pointer' }} onClick={copiaColore}>{rgbToHex(...rgb)}</span>
     </div>
   )
 }
